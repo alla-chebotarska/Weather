732cc29afbd112ea3762d1e224890b04add4eca6
Alla Chebotarska <alla.chebotarska@gmail.com>
2019-12-09 21:07:24 +0100

    week forecast in progress

--- script/weather.js
+++ script/weather.js
@@ -1,3 +1,4 @@
+
 function findCity() {
     let city = document.getElementById('cityInput').value;
     if (city === '') {
@@ -9,36 +10,68 @@ function findCity() {
     }
 }
 
-function getWeatherForCity(city) {
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=607cd3f34435efcbf7e597f25330293b&units=metric';
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false); // false for synchronous request
-    xmlHttp.send();
-    if (xmlHttp.status === 200) {
-        let weather = JSON.parse(xmlHttp.responseText);
-        return weather;
-    } else {
-        document.getElementById('errorAlert').classList.remove('hidden');
-        document.getElementById('weather').classList.add('hidden');
-        return null;
-    }
+function showWeather(weatherObject) {
+    showCurrentWeather(weatherObject);
+    showWeekForecast(weatherObject);
 }
 
-function showWeather(weatherObject) {
+function showCurrentWeather(weatherObject) {
     document.getElementById('errorAlert').classList.add('hidden');
     document.getElementById('weather').classList.remove('hidden');
-    let description = weatherObject.weather[0].description;
+    let description = weatherObject.list[0].weather[0].description;
     document.getElementById('description').innerText = description;
-    let temp = Math.round(weatherObject.main.temp);
+    let temp = Math.round(weatherObject.list[0].main.temp);
     document.getElementById('temp').innerHTML = temp + ' &deg;C';
-    let city = weatherObject.name;
+    let city = weatherObject.city.name;
     document.getElementById('location').innerText = city;
-    showWeatherImage(weatherObject.weather[0].icon);
+    showWeatherImage(weatherObject.list[0].weather[0].icon);
+    let humidity = weatherObject.list[0].main.humidity;
+    document.getElementById('humidity').innerText = 'Humidity: ' + humidity + ' %';
+    let windSpeed = weatherObject.list[0].wind.speed;
+    document.getElementById('windSpeed').innerText = 'Wing speed: ' + windSpeed + ' m/c';
+}
+
+function showWeekForecast(weatherObject) {
+    let weatherList = weatherObject.list;
+    let forecastArray = [];
+    let tmpArray = [];
+    weatherList.forEach(value => {
+        if (tmpArray.length === 0) {
+            tmpArray.push(value);
+        } else {
+            let date = extractDate(value);
+            if (extractDate(tmpArray[0]) === date) {
+                tmpArray.push(value);
+            } else {
+                forecastArray.push(tmpArray);
+                tmpArray = [value];
+            }
+        }
+    });
+    console.log(forecastArray);
+}
+
+function extractDate(value) {
+    return value.dt_txt.substr(0, 10);
 }
 
 function showWeatherImage(imageId) {
-    let cutImageId = imageId.substr(0, 2);
-    let imgSrc = 'images/weatherImg/' + cutImageId + '.svg';
-    document.getElementById('weatherImage').src = imgSrc ;
+    let ImageForShow;
+    if (imageId.startsWith('01') || imageId.startsWith('02')) {
+        ImageForShow = imageId;
+        console.log('imageId')
+    } else {
+        ImageForShow = imageId.substr(0, 2);
+    }
+    let imgSrc = 'images/weatherImg/' + ImageForShow + '.svg';
+    document.getElementById('weatherImage').src = imgSrc;
 }
 
+
+let inputField = document.getElementById('cityInput');
+inputField.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("findCityBtn").click();
+    }
+});
